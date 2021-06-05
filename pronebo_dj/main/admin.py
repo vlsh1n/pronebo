c1fc986cde2c4d8455bca5668f5e90074a3ad808
Vladislav Voloshin <voloshinw@gmail.com>
2021-06-05 16:00:15 +0300

    edited admin page for creating and editing items

--- pronebo_dj/main/admin.py
+++ pronebo_dj/main/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 # Register your models here.
+
+from .models import *
+
+
+class ItemAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    save_on_top = True
+    form = ItemAdminForm
+    list_display = ('id', 'title',)
+    list_display_links = ('id', 'title')
+
+
+admin.site.register(Item, ItemAdmin)
--- pronebo_dj/main/models.py
+++ pronebo_dj/main/models.py
@@ -4,13 +4,15 @@ from django.db import models
 
 
 class Item(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Название')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
-    content = models.TextField(blank=True)
+    content = models.TextField(blank=True, verbose_name='Описание')
     photo = models.ImageField(upload_to='photos/', blank=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = 'Услуга(-у)'
+        verbose_name_plural = 'Услуг(-и)'
         ordering = ['title']
