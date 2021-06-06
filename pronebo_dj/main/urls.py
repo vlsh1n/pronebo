614726a6231cd79d5a2590970f5ab1845f5f35fb
Vladislav Voloshin <voloshinw@gmail.com>
2021-06-06 15:31:55 +0300

    added js script to base template | deleted unusable templates | added url and viewer for single product page and changed template logic for this page

--- pronebo_dj/main/urls.py
+++ pronebo_dj/main/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('faq/', faq, name='faq'),
+    path('items/<slug:slug>', get_slug)
 ]
--- pronebo_dj/main/views.py
+++ pronebo_dj/main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
-def index(request):
+def index(request, slug):
     return render(request, 'main/index.html')
 
 
 def faq(request):
     return render(request, 'main/faq.html')
+
+
+def get_slug(request, slug):
+    slug = Item.objects.get(slug=slug)
+    return render(request, 'main/shop-single.html', {'slug': slug})
