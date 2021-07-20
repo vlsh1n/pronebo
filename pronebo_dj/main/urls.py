d9f10a84ed9b387c3c78dfbb6da495be860f1e1b
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-20 14:42:34 +0300

    deleted unusable paths in urls in main app

--- pronebo_dj/main/urls.py
+++ pronebo_dj/main/urls.py
@@ -5,7 +5,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('faq/', faq, name='faq'),
-    path('services/<slug:slug>', service, name='services'),
-    path('contact/', contact_form, name='contact'),
-    path('test/', test, name='test'),
+    path('services/<slug:slug>', service, name='services')
 ]
