183573d872620b6f73ed9bbefe6b10c7676f35ee
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-20 14:42:53 +0300

    remade logic for contact form

--- pronebo_dj/main/forms.py
+++ pronebo_dj/main/forms.py
@@ -1,12 +1,22 @@
 from django import forms
 
-from .models import Item
+from .models import Item, Packs
 
 
 # Creating model for order form
 class OrderForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    email = forms.EmailField()
-    number = forms.NumberInput()
-    service = forms.ModelChoiceField(empty_label='Выберите услугу из списка', queryset=Item.objects.all())
-    message = forms.CharField(max_length=500, required=False)
+    name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+        'placeholder': 'Как вас зовут?'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Ваш Email'
+    }))
+    number = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Ваш контактный номер'
+    }))
+    service = forms.ModelChoiceField(empty_label='Выберите услугу из списка', queryset=Item.objects.all(),
+                                     widget=forms.Select())
+    pack = forms.ModelChoiceField(queryset=Packs.objects.all(), widget=forms.RadioSelect)
+    message = forms.CharField(max_length=500, required=False, widget=forms.Textarea(attrs={
+        'placeholder': 'Комментарий'
+    }))
