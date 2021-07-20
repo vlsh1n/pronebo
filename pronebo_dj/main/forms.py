5f26efbd4f3e3967dec407b00d94299d966c7800
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-20 14:57:04 +0300

    deleted labels in form

--- pronebo_dj/main/forms.py
+++ pronebo_dj/main/forms.py
@@ -5,18 +5,18 @@ from .models import Item, Packs
 
 # Creating model for order form
 class OrderForm(forms.Form):
-    name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+    name = forms.CharField(max_length=255, label='', widget=forms.TextInput(attrs={
         'placeholder': 'Как вас зовут?'
     }))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
+    email = forms.EmailField(label='', widget=forms.EmailInput(attrs={
         'placeholder': 'Ваш Email'
     }))
-    number = forms.CharField(widget=forms.TextInput(attrs={
+    number = forms.CharField(label='', widget=forms.TextInput(attrs={
         'placeholder': 'Ваш контактный номер'
     }))
-    service = forms.ModelChoiceField(empty_label='Выберите услугу из списка', queryset=Item.objects.all(),
+    service = forms.ModelChoiceField(empty_label='Выберите услугу из списка', label='', queryset=Item.objects.all(),
                                      widget=forms.Select())
-    pack = forms.ModelChoiceField(queryset=Packs.objects.all(), widget=forms.RadioSelect)
-    message = forms.CharField(max_length=500, required=False, widget=forms.Textarea(attrs={
+    pack = forms.ModelChoiceField(queryset=Packs.objects.all(), label='', widget=forms.RadioSelect)
+    message = forms.CharField(max_length=500, label='', required=False, widget=forms.Textarea(attrs={
         'placeholder': 'Комментарий'
     }))
